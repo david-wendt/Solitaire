f22e4f35315ab8026956b37e74744b3d62ca10e7
david-wendt <dwendt@stanford.edu>
2025-01-21 10:47:05 +0100

    tidy up code

--- main.go
+++ main.go
@@ -4,39 +4,22 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"strings"
 	"strconv"
+	"strings"
 
 	"solitaire/deck"
 	"solitaire/game"
 )
 
-const nStacks int = game.NStacks
-
-// var testing bool = true  
-var testing bool = false 
-
-
-func test() {
-	fmt.Println("Hello world!")
-}
-
 func main() {
-	if testing {
-		test()
-		return
-	}
 
 	deck := deck.NewDeck()
-	// fmt.Println(deck)
 	deck.Shuffle()
-	// fmt.Println(deck)
 
 	game := game.NewGame(deck)
-	// fmt.Printf("%+v\n", game)
 
-	game.Display(false)
-	
+	game.Display(true) // `true` to hide hidden cards
+
 	for {
 		fmt.Print("Enter a move: ")
 		reader := bufio.NewReader(os.Stdin)
@@ -51,7 +34,7 @@ func main() {
 			fmt.Println("\t\tOmit <n> to move all visible cards")
 			fmt.Println("\tfollow with `<src> t` to move from stack <src> (in tableau) to top (foundation)")
 			fmt.Println("\tfollow with `t <src> <dst>` to move from top (foundation) stack <src> to stack <dst> (in tableau)")
-			continue 
+			continue
 		}
 
 		fields := strings.Fields(move)
@@ -63,34 +46,34 @@ func main() {
 			if move == "a t" {
 				err = game.MoveAvailToTop()
 			} else if fields[0] == "a" {
-				dst,ok := strconv.Atoi(fields[1])
+				dst, ok := strconv.Atoi(fields[1])
 				if ok != nil {
 					fmt.Printf("Must follow `a` with int. strconv.Atoi error: %v", ok)
 				} else {
 					err = game.MoveFromAvail(dst)
 				}
-			} else if fields[0] == "t"  && len(fields) > 2{
-				src,ok1 := strconv.Atoi(fields[1])
-				dst,ok2 := strconv.Atoi(fields[2])
+			} else if fields[0] == "t" && len(fields) > 2 {
+				src, ok1 := strconv.Atoi(fields[1])
+				dst, ok2 := strconv.Atoi(fields[2])
 				if ok1 == nil && ok2 == nil {
 					err = game.MoveFromTop(src, dst)
 				} else {
 					fmt.Printf("strconv.Atoi error(s): %v, %v", ok1, ok2)
 				}
 			} else if len(fields) > 1 && fields[1] == "t" {
-				src,ok := strconv.Atoi(fields[0])
+				src, ok := strconv.Atoi(fields[0])
 				if ok == nil {
 					err = game.MoveToTop(src)
 				} else {
 					fmt.Printf("strconv.Atoi error: %v", ok)
 				}
 			} else if len(fields) > 1 {
-				src,ok1 := strconv.Atoi(fields[0])
-				dst,ok2 := strconv.Atoi(fields[1])
+				src, ok1 := strconv.Atoi(fields[0])
+				dst, ok2 := strconv.Atoi(fields[1])
 				n := 0
 				var ok3 error = nil
 				if len(fields) > 2 {
-					n,ok3 = strconv.Atoi(fields[2])
+					n, ok3 = strconv.Atoi(fields[2])
 				}
 				if ok1 == nil && ok2 == nil && ok3 == nil {
 					err = game.Move(src, dst, n)
@@ -100,7 +83,7 @@ func main() {
 			} else {
 				fmt.Println("Invalid move command! See `help`.")
 			}
-			
+
 			if err != nil {
 				fmt.Printf("Move error: %v\n", err)
 			}
